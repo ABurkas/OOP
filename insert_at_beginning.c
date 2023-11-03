97874803713800b8f06c826bab70b8374df46ef7
Albert Burkas <albert.burkas@gmail.com>
2023-11-03 18:01:39 +0100

    New

--- insert_at_beginning.c
+++ insert_at_beginning.c
@@ -1,21 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct Node{
-    int data;
-    struct Node* next;
-};
 
-struct Node* head;
 int main()
-{
-    head = NULL;
-    printf("How many numbers?\n");
-    int n,i,x;
-    scanf("%d", &n);
-    for(i = 0; i < n; i++){
-        printf("Enter the number\n");
-    }
-
+{   
+    int a =5;
+    int *p;
+    p = &a;
+    printf("%d", p);
 
 
     return 0;
