118fdeaeea058e9872c99b3bdf1d25f504d8a996
Albert Burkas <albert.burkas@gmail.com>
2023-11-04 23:16:05 +0100

    New

--- insert_at_beginning.c
+++ insert_at_beginning.c
@@ -3,10 +3,15 @@
 
 int main()
 {   
-    int a =5;
+    int a = 1025;
     int *p;
     p = &a;
-    printf("%d", p);
+    printf("%d\n", sizeof(int));
+    printf("Address = %d, value = %d", p,*p);
+    char *p0;
+    p0 = (char*)p;
+    printf("size of char is %d bytes\n", sizeof(char));
+    printf("Address = %d, value = %d\n", p0, *p0);
 
 
     return 0;
